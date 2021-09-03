5520274a4f0c50be7d4d4f620242e3b6026c6af1
Karthik Vasu <kvasu2@illinois.edu>
2021-09-03 14:58:15 -0500

    changed files to input

--- PDF-MergeWithBlanks/PDF_merger.py
+++ PDF-MergeWithBlanks/PDF_merger.py
@@ -3,10 +3,10 @@ import PyPDF2
 import os
 
 
-# List all files in a directory using os.listdir
+
 basepath = os.path.dirname(os.path.abspath(__file__))
 
-files_path = os.path.join(basepath,'files')
+files_path = os.path.join(basepath,'input')
 
 blank_path = os.path.join(basepath,'blank.pdf')
 
@@ -25,11 +25,9 @@ pdf_merger = PyPDF2.PdfFileMerger()
 for entry in os.listdir(files_path):
     file_path = os.path.join(files_path, entry)
     if os.path.isfile(file_path):
-        #files.append(open(file_path, 'rb'))
         
         pdf_merger.append(file_path)
         
-        # creating a pdf File object of original pdf
         pdfReader = PyPDF2.PdfFileReader(file_path)
         
         if pdfReader.numPages%2 ==1:
@@ -43,24 +41,3 @@ newFile = open(output_path, 'wb')
 pdf_merger.write(newFile)
 
 pdf_merger.close()
-
-
-
-
-# creating a pdf file object
-#pdfFileObj = open('example.pdf', 'rb')
- 
-# creating a pdf reader object
-#pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
- 
-# printing number of pages in pdf file
-#print(pdfReader.numPages)
- 
-# creating a page object
-#pageObj = pdfReader.getPage(0)
- 
-# extracting text from page
-#print(pageObj.extractText())
- 
-# closing the pdf file object
-#pdfFileObj.close()
