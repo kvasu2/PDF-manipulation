f98556286ee72ef1fc43069da619de3b7d32a04b
Karthik Vasu <kvasu2@illinois.edu>
2021-11-24 12:58:19 -0600

    Added PDF-Spliter-Mass

---
+++ PDF-Splitter-Mass/Readme.txt
@@ -0,0 +1,9 @@
+Copy the pdfs to "input" folder
+
+Open terminal and type the following (or double click on pdf_splitter.py on Windows)
+
+>> python pdf_splitter.py
+
+Then input the start and end page numbers when prompted.
+
+All the pdfs will be trimmed to the specified pages
---
+++ PDF-Splitter-Mass/pdf_splitter_mass.py
@@ -0,0 +1,39 @@
+# pdf_splitter.py
+
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+ 
+ 
+def pdf_splitter(path,strt,ed):
+	fname = os.path.splitext(os.path.basename(path))[0]
+	
+	pdf = PdfFileReader(path)
+	if pdf.isEncrypted:
+		try:
+			pdf.decrypt('')
+		except:
+			pdf.decrypt(input("Enter password: "))
+			
+	pdf_writer = PdfFileWriter()
+	for page in range(strt-1,ed):
+		pdf_writer.addPage(pdf.getPage(page))
+	
+	output_filename = '{}.pdf'.format(fname)
+ 
+	with open(output_filename, 'wb') as out:
+		pdf_writer.write(out)
+	
+	print('Created: {}'.format(output_filename))	
+	
+
+basepath = os.path.dirname(os.path.abspath(__file__))
+
+inp_path = os.path.join(basepath,'input')
+
+srt=input("Start Page: ")
+edd=input("End Page: ")
+
+for inp in os.listdir(inp_path):
+    file_path = os.path.join(inp_path, inp)
+    pdf_splitter(file_path,int(srt),int(edd))
+
